caf97651b39ed0708deef9bd91e2d56cf98903a4
rubin <devnull@localhost>
2008-11-15 06:05:30 +0000

    more work toward python scripting

--- src/mod-python.c
+++ src/mod-python.c
@@ -35,7 +35,8 @@
 
 
 static const struct message_entry msgtab[] = {
-    { "MSMSG_FOO", "foo there $b%s$b." },
+    { "PYMSG_RELOAD_SUCCESS", "Reloaded Python scripts successfully." },
+    { "PYMSG_RELOAD_FAILED", "Error reloading Python scripts." },
     { NULL, NULL } /* sentenal */
 };
 
@@ -43,13 +44,44 @@ static struct log_type *PY_LOG;
 const char *python_module_deps[] = { NULL };
 static struct module *python_module;
 
+PyObject *base_module = NULL; /* Base python handling library */
 
-/* Called on shutdown of the module */
-static void
-python_cleanup(void)
-{
-    return;
+int python_call_func(char *function, ...) {
+    /* TODO: get arguments, pass through to python function */
+    PyObject *pFunc, *pValue;
+    if(base_module != NULL) {
+        log_module(PY_LOG, LOG_INFO, "%s returned successfully", function);
+	pFunc = PyObject_GetAttrString(base_module, function);
+	/* pFunc is a new reference */
+	if(pFunc && PyCallable_Check(pFunc)) {
+	    pValue = PyObject_CallObject(pFunc, NULL);
+	    if(pValue != NULL) {
+		log_module(PY_LOG, LOG_INFO, "%s returned successfully", function);
+		Py_DECREF(pValue);
+		return 1;
+	    }
+	    else {
+		Py_DECREF(pFunc);
+		Py_DECREF(base_module);
+		/* TODO: instead of print errors, get them as strings
+		 * and deal with them with normal x3 log system. */
+		PyErr_Print();
+		log_module(PY_LOG, LOG_WARNING, "call to %s failed", function);
+		return 0;
+	    }
+	}
+	else {
+	    if(PyErr_Occurred())
+		PyErr_Print();
+	    log_module(PY_LOG, LOG_WARNING, "function %s not found or uncallable", function);
+	    return 0;
+	}
+    } 
+    else {
+	return 0;
+    }
 }
+
 
 static int
 python_handle_join(struct modeNode *mNode)
@@ -57,36 +89,84 @@ python_handle_join(struct modeNode *mNode)
     struct userNode *user = mNode->user;
     struct chanNode *channel = mNode->channel;
 
+    log_module(PY_LOG, LOG_INFO, "python module handle_join");
+    python_call_func("handle_join");
     PyRun_SimpleString("print 'Someone joined a channel somewhere'");
     return 0;
+}
+
+int python_load() {
+    PyObject *pName;
+
+    setenv("PYTHONPATH", "/home/rubin/afternet/services/x3/x3-run/", 1);
+    Py_Initialize();
+    /* TODO: get "mod-python" from x3.conf */
+    pName = PyString_FromString("mod-python");
+    base_module = PyImport_Import(pName);
+    Py_DECREF(pName);
+    if(base_module != NULL) {
+	return python_call_func("handle_init");
+    }
+    else {
+	PyErr_Print();
+	log_module(PY_LOG, LOG_WARNING, "Failed to load mod-python.py");
+	return 0;
+    }
+}
+
+/* Called after X3 is fully up and running */
+int
+python_finalize(void) {
+
+    PyRun_SimpleString("print 'Hello, World of Python!'");
+    log_module(PY_LOG, LOG_INFO, "python module finalize");
+
+    return 1;
+}
+
+/* Called on shutdown of the module */
+static void
+python_cleanup(void)
+{
+    log_module(PY_LOG, LOG_INFO, "python module cleanup");
+    Py_Finalize(); /* Shut down python enterpriter */
+    return;
+}
+
+static MODCMD_FUNC(cmd_reload) {
+    log_module(PY_LOG, LOG_INFO, "Shutting python down");
+    python_cleanup();
+    log_module(PY_LOG, LOG_INFO, "Loading python stuff");
+    if(python_load()) {
+	 reply("PYMSG_RELOAD_SUCCESS");
+    }
+    else {
+	reply("PYMSG_RELOAD_FAILED");
+    }
+    return 1;
 }
 
 /* Called on init of the module during startup */
 int python_init(void) {
 
-
     PY_LOG = log_register_type("Python", "file:python.log");
-    reg_join_func(python_handle_join);
+    python_module = module_register("python", PY_LOG, "mod-python.help", NULL);
+    log_module(PY_LOG, LOG_INFO, "python module init");
+    message_register_table(msgtab);
+
 /*
     reg_auth_func(python_check_messages);
     reg_handle_rename_func(python_rename_account);
     reg_unreg_func(python_unreg_account);
     conf_register_reload(python_conf_read);
-    reg_exit_func(python_cleanup);
     saxdb_register("python", python_saxdb_read, python_saxdb_write);
-    python_module = module_register("python", MS_LOG, "mod-python.help", NULL);
     modcmd_register(python_module, "send",    cmd_send,    3, MODCMD_REQUIRE_AUTHED, NULL);
 */
-    Py_Initialize();
+    modcmd_register(python_module, "reload",  cmd_reload,  1,  MODCMD_REQUIRE_AUTHED, "flags", "+oper", NULL);
+    reg_join_func(python_handle_join);
+    reg_exit_func(python_cleanup);
 
-    return 1;
-}
-
-int
-python_finalize(void) {
-
-    PyRun_SimpleString("print 'Hello, World of Python!'");
-
+    python_load();
     return 1;
 }
 
---
+++ src/mod-python.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+print "This is mod-python.py"
+
+def handle_init():
+    print "This is x3init in python"
+
+def handle_join():
+    print "This is handle_join() in python"
+
