a2c8c5750d48ffbd1442fa31291f074fc201e714
rubin <devnull@localhost>
2008-11-16 22:36:11 +0000

    more python work

--- src/mod-python.c
+++ src/mod-python.c
@@ -33,6 +33,15 @@
 #include "sendmail.h"
 #include "timeq.h"
 
+/* TODO notes
+ *
+ * - Impliment most of proto-p10 irc_* commands for calling from scripts
+ * - Impliment functions to look up whois, channel, account, and reg-channel info for scripts
+ * - Impliment x3.conf settings for python variables like include path, etc.
+ * - kod-python.py calls for everything you can reg_ a handler for in x3
+ * - Some kind of system for getting needed binds bound automagicaly to make it easier
+ *   to run peoples scripts and mod-python in general.
+ */
 
 static const struct message_entry msgtab[] = {
     { "PYMSG_RELOAD_SUCCESS", "Reloaded Python scripts successfully." },
@@ -46,39 +55,108 @@ static struct module *python_module;
 
 PyObject *base_module = NULL; /* Base python handling library */
 
-int python_call_func(char *function, ...) {
+static PyObject*
+emb_dump(PyObject *self, PyObject *args)
+{
+    char *buf;
+    int ret = 0;
+    char linedup[MAXLEN];
+
+    if(!PyArg_ParseTuple(args, "s:dump", &buf ))
+        return NULL;
+    safestrncpy(linedup, buf, sizeof(linedup));
+    if(parse_line(linedup, 1)) {
+        irc_raw(buf);
+        ret = 1;
+    }
+    return Py_BuildValue("i", ret);
+}
+
+static PyObject*
+emb_send_target_privmsg(PyObject *self, PyObject *args)
+{
+    int ret = 0;
+    char *servicenick;
+    char *channel;
+    char *buf;
+
+    struct service *service;
+
+    if(!PyArg_ParseTuple(args, "sss:reply", &servicenick, &channel, &buf ))
+        return NULL;
+    if(!(service = service_find(servicenick))) {
+        /* TODO: generate python exception here */
+        return 0;
+    }
+    send_target_message(5, channel, service->bot, "%s", buf);
+    return Py_BuildValue("i", ret);
+}
+
+static PyMethodDef EmbMethods[] = {
+    {"dump", emb_dump, METH_VARARGS, "Dump raw P10 line to server"},
+    {"send_target_privmsg", emb_send_target_privmsg, METH_VARARGS, "Send a message to somewhere"},
+    {NULL, NULL, 0, NULL}
+};
+
+
+int python_call_func(char *function, char *args[], size_t argc) {
     /* TODO: get arguments, pass through to python function */
     PyObject *pFunc, *pValue;
+    PyObject *pArgs = NULL;
     if(base_module != NULL) {
-        log_module(PY_LOG, LOG_INFO, "%s returned successfully", function);
-	pFunc = PyObject_GetAttrString(base_module, function);
-	/* pFunc is a new reference */
-	if(pFunc && PyCallable_Check(pFunc)) {
-	    pValue = PyObject_CallObject(pFunc, NULL);
-	    if(pValue != NULL) {
-		log_module(PY_LOG, LOG_INFO, "%s returned successfully", function);
-		Py_DECREF(pValue);
-		return 1;
-	    }
-	    else {
-		Py_DECREF(pFunc);
-		Py_DECREF(base_module);
-		/* TODO: instead of print errors, get them as strings
-		 * and deal with them with normal x3 log system. */
-		PyErr_Print();
-		log_module(PY_LOG, LOG_WARNING, "call to %s failed", function);
-		return 0;
-	    }
-	}
-	else {
-	    if(PyErr_Occurred())
-		PyErr_Print();
-	    log_module(PY_LOG, LOG_WARNING, "function %s not found or uncallable", function);
-	    return 0;
-	}
+        log_module(PY_LOG, LOG_INFO, "Attempting to run python function %s", function);
+        pFunc = PyObject_GetAttrString(base_module, function);
+        /* pFunc is a new reference */
+        if(pFunc && PyCallable_Check(pFunc)) {
+            size_t i;
+            if(args && argc) {
+                pArgs = PyTuple_New(argc);
+                for(i = 0; i< argc; ++i) {
+                    pValue = PyString_FromString(args[i]);
+                    if(!pValue) {
+                        Py_DECREF(pArgs);
+                        Py_DECREF(pFunc);
+                        log_module(PY_LOG, LOG_INFO, "Unable to convert '%s' to python string", args[i]);
+                        return 0;
+                    }
+                    PyTuple_SetItem(pArgs, i, pValue);
+                }
+            }
+            pValue = PyObject_CallObject(pFunc, pArgs);
+            if(pArgs != NULL)  {
+               Py_DECREF(pArgs);
+            }
+            if(pValue != NULL) {
+                int ret;
+                ret = PyInt_AsLong(pValue);
+                if(ret == -1 && PyErr_Occurred()) {
+                    PyErr_Print();
+                    log_module(PY_LOG, LOG_INFO, "error converting return value of %s to type long. ", function);
+                    ret = 0;
+                }
+                log_module(PY_LOG, LOG_INFO, "%s was run successfully, returned %d.", function, ret);
+                /* TODO: convert pValue to c int, return it below */
+                Py_DECREF(pValue);
+                return ret;
+            }
+            else {
+                Py_DECREF(pFunc);
+                /* TODO: instead of print errors, get them as strings
+                 * and deal with them with normal x3 log system. */
+                PyErr_Print();
+                log_module(PY_LOG, LOG_WARNING, "call to %s failed", function);
+                return 0;
+            }
+        }
+        else {
+            if(PyErr_Occurred())
+                PyErr_Print();
+            log_module(PY_LOG, LOG_WARNING, "function %s not found or uncallable", function);
+            return 0;
+        }
     } 
     else {
-	return 0;
+        return 0;
     }
 }
 
@@ -89,10 +167,15 @@ python_handle_join(struct modeNode *mNode)
     struct userNode *user = mNode->user;
     struct chanNode *channel = mNode->channel;
 
+
     log_module(PY_LOG, LOG_INFO, "python module handle_join");
-    python_call_func("handle_join");
-    PyRun_SimpleString("print 'Someone joined a channel somewhere'");
-    return 0;
+    if(!channel||!user) {
+        return 0;
+    }
+    else {
+        char *args[] = {channel->name, user->nick};
+        return python_call_func("handle_join", args, 2);
+    }
 }
 
 int python_load() {
@@ -100,17 +183,20 @@ int python_load() {
 
     setenv("PYTHONPATH", "/home/rubin/afternet/services/x3/x3-run/", 1);
     Py_Initialize();
+    Py_InitModule("svc", EmbMethods);
+    PyRun_SimpleString("import svc");
     /* TODO: get "mod-python" from x3.conf */
     pName = PyString_FromString("mod-python");
     base_module = PyImport_Import(pName);
     Py_DECREF(pName);
     if(base_module != NULL) {
-	return python_call_func("handle_init");
+        python_call_func("handle_init", NULL, 0);
+        return 1;
     }
     else {
-	PyErr_Print();
-	log_module(PY_LOG, LOG_WARNING, "Failed to load mod-python.py");
-	return 0;
+        PyErr_Print();
+        log_module(PY_LOG, LOG_WARNING, "Failed to load mod-python.py");
+        return 0;
     }
 }
 
@@ -138,11 +224,19 @@ static MODCMD_FUNC(cmd_reload) {
     python_cleanup();
     log_module(PY_LOG, LOG_INFO, "Loading python stuff");
     if(python_load()) {
-	 reply("PYMSG_RELOAD_SUCCESS");
+         reply("PYMSG_RELOAD_SUCCESS");
     }
     else {
-	reply("PYMSG_RELOAD_FAILED");
+        reply("PYMSG_RELOAD_FAILED");
     }
+    return 1;
+}
+
+static MODCMD_FUNC(cmd_python) {
+    
+    char *msg;
+    msg = unsplit_string(argv + 1, argc - 1, NULL);
+    PyRun_SimpleString(msg);
     return 1;
 }
 
@@ -163,6 +257,7 @@ int python_init(void) {
     modcmd_register(python_module, "send",    cmd_send,    3, MODCMD_REQUIRE_AUTHED, NULL);
 */
     modcmd_register(python_module, "reload",  cmd_reload,  1,  MODCMD_REQUIRE_AUTHED, "flags", "+oper", NULL);
+    modcmd_register(python_module, "python",  cmd_python,  2,  MODCMD_REQUIRE_AUTHED, "flags", "+oper", NULL);
     reg_join_func(python_handle_join);
     reg_exit_func(python_cleanup);
 
--- src/mod-python.py
+++ src/mod-python.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python
 
+
+# TODO notes:
+#
+# - impliment handle_* functions for everything x3 has register fetaures for
+# - impliment script load/unload for user scripts.
+#       - load a script via this script. 
+#       - script calls functions from here to set its functions up for calling on various actions
+# - provide helper functions for subscripts to save settings attached to users/chanels
+# - provide helper functions for scripts to do common things like msg a person or a channel,
+#   reply, etc.
+
+import svc
+
 print "This is mod-python.py"
 
 def handle_init():
     print "This is x3init in python"
+    return 0
 
-def handle_join():
+def handle_join(channel, user):
     print "This is handle_join() in python"
+    print "   %s joined %s"%(user, channel)
+    svc.send_target_privmsg("x3", channel, "Welcome to %s %s!"%(channel, user))
+    return 0
+
 
