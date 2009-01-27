0bcdc113fbcfd032dea8dad31a43dbaea3f88662
rubin <devnull@localhost>
2009-01-27 01:19:58 +0000

    update test python plugin

--- src/plugins/annoy/plugin.py
+++ src/plugins/annoy/plugin.py
@@ -8,15 +8,28 @@ class Annoy:
         self.handler = handler
         self.name = "annoy"
 
-        irc.send_target_privmsg("O3", "#TheOPS", "%s is loaded"%self.name)
-        handler.addhook("join", self.on_join, "foobar")
+#        irc.send_target_privmsg("O3", "#TheOPS", "%s is loaded"%self.name)
+#        handler.addhook("join", self.on_join, "foobar")
         handler.addcommand(self.name, "dance", self.dance)
         self.test = "footest"
 
-    def on_join(self, irc, channel, nick):
-        irc.send_target_privmsg("x3", channel, "%s joined %s:%s "%(nick, channel, self.test))
+#    def on_join(self, irc, channel, nick):
+#        irc.send_target_privmsg("x3", channel, "%s joined %s:%s "%(nick, channel, self.test))
 
     def dance(self, irc, args):
-        irc.reply("Ok, %s, we can dance %s."%(irc.caller, args))
+        nick = irc.caller
+        user = svc.get_user(nick)
+
+        reply = "Ok,"
+        if(user and "account" in user):
+           reply +=  " Mr. %s"%user["account"]
+
+        reply += " we can dance"
+        if(len(args)):
+            reply += " "
+            reply += args
+        reply += "."
+
+        irc.reply(reply)
 
 Class = Annoy
