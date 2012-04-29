4179bc63b833da74cf8a7b39d817a359b7285249
Matthew Beeching <jobe@mdbnet.co.uk>
2012-04-29 18:24:19 +0100

    Replaced ldap_get_values() with ldap_get_values_len()

--- src/x3ldap.c
+++ src/x3ldap.c
@@ -170,19 +170,20 @@ int ldap_search_user(char *account, LDAPMessage **entry)
 int ldap_get_user_info(const char *account, char **email) 
 {
     int rc;
-    char **value;
+    struct berval **value;
     LDAPMessage *entry, *res;
     if(email)
       *email = NULL;
     if( (rc = ldap_search_user(account, &res)) == LDAP_SUCCESS) {
         entry = ldap_first_entry(ld, res);
-        value = ldap_get_values(ld, entry, nickserv_conf.ldap_field_email);
+        value = ldap_get_values_len(ld, entry, nickserv_conf.ldap_field_email);
         if(!value) {
            return(LDAP_OTHER);
         }
         if(email)
-          *email = strdup(value[0]);
-        log_module(MAIN_LOG, LOG_DEBUG, "%s: %s\n", nickserv_conf.ldap_field_email, value[0]);
+          *email = strdup(value[0]->bv_val);
+        log_module(MAIN_LOG, LOG_DEBUG, "%s: %s\n", nickserv_conf.ldap_field_email, value[0]->bv_val);
+        ldap_value_free_len(value);
         /*
         value = ldap_get_values(ld, entry, "description");
         log_module(MAIN_LOG, LOG_DEBUG, "Description: %s\n", value[0]);
